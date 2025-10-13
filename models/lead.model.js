331f0c9513734f89ceb1b69263e44f48db16b2d4
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-10-13 11:44:16 +0530

    modified lead model

--- models/lead.model.js
+++ models/lead.model.js
@@ -22,7 +22,7 @@ const leadSchema = new mongoose.Schema({
   salesAgent: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "SalesAgent", // Reference to salesAgent model
-    required: [true, "Sales Agent is required"],
+    // required: [true, "Sales Agent is required"],
   },
 
   status: {
